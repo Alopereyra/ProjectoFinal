485d291212c7da0d08b5564b25ff2786c667b209
Alejandro <alopereyra79@gmail.com>
2022-10-18 18:58:12 -0300

    agrego modelos

--- ProjectoFinal/blogcoder/models.py
+++ ProjectoFinal/blogcoder/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    profesion = models.CharField(max_length=30)
+    
+class Articulos(models.Model):
+    titulo = models.CharField(max_length=30)
+    texto = models.CharField(max_length=1000)
+    fecha = models.DateField(null = True)
+
+class Seccion(models.Model):
+    nombre = models.CharField(max_length=30)
+    
+    
+    
